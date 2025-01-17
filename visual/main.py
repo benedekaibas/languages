2c9c12b935dc14bd0b62f61adcf43accfec6229d
benedekaibas <kaibas.benedek02@gmail.com>
2025-01-17 13:46:01 -0500

    folder & file created. main file started

---
+++ visual/main.py
@@ -0,0 +1,9 @@
+"""Main file for creating visual representation for the results."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class Visual():
+    """Storing and calling the functions."""
+    def __init__(self):
+        pass
